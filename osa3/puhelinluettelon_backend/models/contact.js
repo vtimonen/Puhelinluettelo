1233d3920ffc8a5ae8093949e3dbb3097e22c6f3
Valtteri Timonen <vtimonen@hotmail.com>
2026-01-18 16:42:11 +0200

    added eslint

--- osa3/puhelinluettelon_backend/models/contact.js
+++ osa3/puhelinluettelon_backend/models/contact.js
@@ -6,32 +6,32 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-    },
-    number: {
-        type: String,
-        minlength: 10,
-        required: true,
-    }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 10,
+    required: true,
+  }
 })
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Contact', contactSchema)
